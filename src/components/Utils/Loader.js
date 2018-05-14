c10db1c57afbb8f6476c052bd579a7448724bc4f
Neelesh Roy <neeleshroy.2013@gmail.com>
2018-05-14 22:14:37 +0530

    loader component

--- src/components/Utils/Loader.js
+++ src/components/Utils/Loader.js
@@ -3,9 +3,7 @@ import React, { Component } from 'react'
 class Loader extends Component {
   render() {
     return (
-      <div className="loader">
-        
-      </div>
+      <div className="loader"></div>
     )
   }
 }
