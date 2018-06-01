0c1e6d3f6faaa5ce817db6b8a587e2bd50f61845
Neelesh Roy <neeleshroy.2013@gmail.com>
2018-06-01 11:05:11 +0530

    temp

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
-import Header from './components/Header'
-import Characters from './components/Characters'
+import Header from './components/Header';
+import Characters from './components/Characters';
+import Home from './components/Home';
 
 class App extends Component {
-  render() {
-    return (
-      <BrowserRouter>
-        <div className="app">
-          <Header />
-          <Route path = "/characters" component={ Characters } />
-        </div>
-      </BrowserRouter>
-    );
-  }
+	render() {
+		return (
+			<BrowserRouter>
+				<div className="app">
+					<Header />
+					<Route path="/" exact component={Home} />
+					<Route path="/characters" exact component={Characters} />
+				</div>
+			</BrowserRouter>
+		);
+	}
 }
 
 export default App;
