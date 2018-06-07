68f996eb0b0d32606437fea07f5ff233317692a9
Neelesh Roy <neeleshroy.2013@gmail.com>
2018-06-07 19:04:42 +0530

    Completed Single functionality

--- src/components/Single.js
+++ src/components/Single.js
@@ -1,13 +1,35 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
-const propTypes = {};
-
-const defaultProps = {};
+const getCarouselData = (items) => (
+	<div>
+		{items.map((item) => (
+			<div key={item.resourceURI}>
+				<a href={item.resourceURI}> {item.name}</a>
+			</div>
+		))}
+	</div>
+);
 
 export default function Single(props) {
-	return <img src={`${props[0].thumbnail.path}.${props[0].thumbnail.extension}`} alt="" />;
+	return (
+		<div>
+			<img src={`${props[0].thumbnail.path}.${props[0].thumbnail.extension}`} alt="" />
+			<div>Name: {props[0].name}</div>
+			<div>
+				Detailed Insights:
+				<ul>
+					{props[0].urls.map((item) => (
+						<li key={item.url}>
+							<a href={item.url} target="_blank">
+								{item.type}
+							</a>
+						</li>
+					))}
+				</ul>
+			</div>
+			<div>Description: {props[0].description !== '' ? props[0].description : 'N/A'}</div>
+			<div>Series: {props[0].series.available > 0 ? getCarouselData(props[0].series.items) : 'N/A'}</div>
+			<div>Stories: {props[0].stories.available > 0 ? getCarouselData(props[0].stories.items) : 'N/A'}</div>
+		</div>
+	);
 }
-
-Single.propTypes = propTypes;
-Single.defaultProps = defaultProps;
