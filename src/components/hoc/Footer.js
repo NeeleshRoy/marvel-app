24e4bc92e6a6fb482722eb6754adf2f56be9ea7f
Neelesh Roy <neeleshroy.2013@gmail.com>
2018-06-08 16:10:27 +0530

    moved styles to styles folder

--- src/components/hoc/Footer.js
+++ src/components/hoc/Footer.js
@@ -1,41 +1,6 @@
 import React from 'react';
-import styled from 'styled-components';
 import { Link } from 'react-router-dom';
-
-const MarvelFooter = styled.footer`
-	min-height: 50px;
-	width: 100%;
-	position: fixed;
-	bottom: 0;
-	background: #000;
-	display: grid;
-	grid-template-columns: 4fr 2fr;
-	align-content: center;
-	justify-content: center;
-`;
-
-const Developer = styled.div`
-	color: white;
-	text-transform: uppercase;
-	font-size: 12px;
-	font-weight: 900;
-	transition: color .2s;
-	&:hover {
-		color: crimson;
-	}
-	margin-left: 30px;
-`;
-
-const Attribution = styled(Link)`
-	color: white;
-	text-transform: uppercase;
-	font-size: 12px;
-	font-weight: 900;
-	transition: color .2s;
-	&:hover {
-		color: crimson;
-	}
-`;
+import { MarvelFooter, Developer, Attribution } from '../../styles/footer.styles';
 
 export default function Footer() {
 	return (
--- src/components/hoc/Header.js
+++ src/components/hoc/Header.js
@@ -1,34 +1,6 @@
 import React from 'react';
-import styled from 'styled-components';
-import { Link } from 'react-router-dom';
 import { header } from '../../constants/static';
-
-const MarvelHeader = styled.header`
-	background: #000;
-	width: 100%;
-	max-height: 60px;
-	display: grid;
-	grid-template-columns: 4fr 1fr;
-	grid-gap: 20px;
-	align-content: center;
-`;
-
-const Logo = styled.h1`
-	margin-left: 20px;
-	color: #f2f2f2;
-`;
-
-const NavItems = styled(Link)`
-  color: #fff;
-  margin-right: 25px;
-  text-transform: uppercase;
-  font-size: 16px;
-  font-weight: 900;
-  transition: color .2s;
-  &:hover{
-    color: crimson;
-  }
-`;
+import { MarvelHeader, Logo, NavItems } from '../../styles/header.styles';
 
 const getNavItems = () =>
 	header.nav.map((item) => (
